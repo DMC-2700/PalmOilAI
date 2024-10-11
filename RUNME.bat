c693441e4273a86be49f852ea06bc3d1d423282d
DMC-2700 <139724586+DMC-2700@users.noreply.github.com>
2024-10-11 16:02:37 +0800

    Update RUNME.bat

--- RUNME.bat
+++ RUNME.bat
@@ -2,5 +2,6 @@ pip install pyrcc5
 pip install TensorFlow
 pip install numpy
 pip install seaborn
+pip install tinker
 start https://www.python.org/downloads/
 start https://code.visualstudio.com/download
